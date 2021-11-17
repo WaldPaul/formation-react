84737cb78d48bdafc54758c33ecdeadcc41fb9c7
WaldPaul <pwaldburger@free.fr>
2021-11-17 14:13:14 +0100

    creation de composants

---
+++ src/App/component/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Button.css"
+/**
+ * Composant button html
+ * @param {object} props props du composant
+ * @returns component render
+ */
+
+const Button = (props) => {
+  return <button className="Button">Benjamin button</button>;
+};
