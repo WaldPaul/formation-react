8d9e97d4de03b672fa0df6bd385b237025168ca3
WaldPaul <pwaldburger@free.fr>
2021-11-19 12:21:47 +0100

    passage de tout sur props pr piocher ds magazin

--- src/App/App.js
+++ src/App/App.js
@@ -1,40 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
 import Button from "./components/Button/Button";
 import FlexLayout from "./components/FlexLayout/FlexLayout";
 import Header from "./components/Header/Header";
 import MemeForm from "./components/MemeForm/MemeForm";
 import MemeViewer from "./components/MemeViewer/MemeViewer";
 import { REST_ADR, REST_RESSOURCES } from "./config/config.js";
-import store from './store/store';
+import store , { currentInitialState, ressourcesInitialState } from './store/store';
 
 class App extends React.Component {
   counter = 0;
   constructor(props) {
     super(props);
-    this.state = { current: {
-        imageId:0,
-        titre:'1er meme',
-        text:'look behind you',
-        x:370,
-        y:530,
-        fontSize:27,
-        color:"#b2e",
-        fontWeight:'900',
-        underline: true,
-        italic:true,
-        frameX:0,
-        frameY:0,
-    }, 
-    images: []
-  };
   }
-  componentDidMount() {
-    fetch(`${REST_ADR}${REST_RESSOURCES.memes}`)
-      .then(f=>f.json())
-      .then(arr=>this.setState({images:arr}))
-  }
+
   componentDidUpdate() {
-    console.log('%c%s', 'font-size:42pt;color:red;background-color:skyblue', 'le changement est pret et effectif ' + JSON.stringify(this.state));
+    console.log('%c%s', 'font-size:22pt;color:red;background-color:skyblue', 'le changement est pret et effectif ' + JSON.stringify(this.state));
   }
   render() {
     return (
@@ -44,27 +25,37 @@ class App extends React.Component {
           <FlexLayout>
           <MemeViewer 
             meme={
-              this.state.current
+              this.props.current
             } 
             image={
-              this.state.images.find((e)=>e.id===this.state.current.imageId)
+              this.props.images.find((e)=>e.id===this.props.current.imageId)
             }
 
             />
 
-            <MemeForm
+            {/* <MemeForm
               meme={this.state.current} 
               onMemeChange={(meme)=> {
                 this.setState({current: meme})
               }
               }
               images={this.state.images}
-            />
+            /> */}
           </FlexLayout>
         </div>
       </>
     );
   }
 }
-
-export default App;
+function mapStateToProps(state,own){
+  return {
+    ...own,
+    current:state.current,
+    images:state.ressources.images
+  }
+}
+function mapDispatchToProps(dispatch)
+{
+  return{}
+}
+export default connect(mapStateToProps,mapDispatchToProps)(App);
